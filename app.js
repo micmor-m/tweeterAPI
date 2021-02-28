73ee64e9505d8f4d9d1be7433cf6224610ab2c9f
mic <micmor.m@gmail.com>
2021-02-28 02:46:12 -0500

    add validation

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const cookieSession = require("cookie-session");
-const saltRounds = 10;
+const { check, body, validationResult } = require("express-validator");
 
 const router = express.Router();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(
-//   cookieSession({
-//     name: "session",
-//     keys: ["abcd", "1234"],
-//   })
-// );
+
 app.use(
   session({
     secret: process.env.SECRET_KEY,
@@ -39,7 +32,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.plugin(passportLocalMongoose);
 
-// // Create User
+// create User
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
@@ -56,52 +49,77 @@ app.get("/register", function (req, res) {
   res.status(200).json({ page: "Register" });
 });
 
-app.post("/register", function (req, res) {
-  User.register(
-    { username: req.body.username },
-    req.body.password,
-    function (err, user) {
+app.post(
+  "/register",
+  //username must be an email
+  body("username").isEmail(),
+  //password must be at least 5 chars long
+  body("password").isLength({ min: 5 }),
+  function (req, res) {
+    //Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.register(
+      { username: req.body.username },
+      req.body.password,
+      function (err, user) {
+        if (err) {
+          console.log("New User not created", err);
+          res.status(500).json({ err });
+        } else {
+          //authenticate user
+          passport.authenticate("local")(req, res, function () {
+            console.log("Created User");
+            res.status(200).json({ user });
+          });
+        }
+      }
+    );
+  }
+);
+
+app.post(
+  "/login",
+  // username must be an email
+  check("username").isEmail(),
+  // password must be at least 5 chars long
+  check("password").isLength({ min: 5 }),
+  function (req, res) {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.login(user, function (err) {
       if (err) {
-        console.log("New User not created", err);
-        res.status(500).json("Internal server error. Try again.");
+        console.log("Login failure", err);
+        res.status(403).json({ err });
       } else {
-        //authenticate user
         passport.authenticate("local")(req, res, function () {
-          console.log("Created User");
-          res.status(200).json({ user });
+          res.status(200).json({ user: req.user });
         });
       }
-    }
-  );
-});
-
-app.post("/login", function (req, res) {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
-  req.login(user, function (err) {
-    if (err) {
-      console.log("Login failure", err);
-      res.status(403).json("Login failure");
-    } else {
-      passport.authenticate("local")(req, res, function () {
-        res.status(200).json({ user: req.user });
-      });
-    }
-  });
-});
+    });
+  }
+);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500).json({ error: "Error" });
-});
+// // error handler
+// app.use(function (err, req, res, next) {
+//   res.status(err.status || 500).json({ error: "Error" });
+// });
 
+// Create DB connection
 const uri = `mongodb+srv://user:${process.env.USER_PW}@cluster0.btpyc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose
@@ -110,6 +128,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then((result) => {
+    //If connection good Start server
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}!`);
     });
